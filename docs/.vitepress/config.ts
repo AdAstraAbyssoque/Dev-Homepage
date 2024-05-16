1aef173606a1d8b1f9069ee58bf8e7965b80af39
HKUSTGZiGem <ustgzigem@outlook.com>
2024-05-16 11:34:16 +0800

    refactor(docs): 重构文档目录结构

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,72 +1,132 @@
-import { defineConfig } from 'vitepress';
+import { defineConfig } from 'vitepress-theme-async/config';
+import taskLists from 'markdown-it-task-lists';
 
 export default defineConfig({
-	outDir: '../dist',
-	lang: 'zh-CN',
-	title: 'Vitepress-Theme-Async',
-	description: '🎈 A simple, lightweight vitepress blog theme',
-	lastUpdated: true,
-	head: [['link', { rel: 'icon', href: '/favicon.svg' }]],
 	cleanUrls: true,
-	themeConfig: {
-		logo: '/logo.svg',
-		search: {
-			provider: 'local',
-		},
-		socialLinks: [
-			{
-				icon: 'github',
-				link: 'https://github.com/MaLuns/vitepress-theme-async',
+	base: '/demosite',
+	outDir: '../dist/demosite',
+	vite: {
+		css: {
+			preprocessorOptions: {
+				less: {},
 			},
-		],
-		editLink: {
-			pattern: 'https://github.com/Maluns/vitepress-theme-async/edit/master/docs/:path',
-			text: '帮助改善此页面！( ￣□￣)/',
 		},
-		nav: [
-			{ text: 'Guide', link: '/guide/' },
-			{ text: 'DemoSites', link: '/demo/' },
+	},
+	markdown: {
+		config(md) {
+			md.use(taskLists);
+		},
+	},
+	title: 'Vitepress Theme Async',
+	description: '一个简洁、优雅、轻量、美观的 vitepress 主题',
+	titleTemplate: ':title | Async',
+	themeConfig: {
+		languages: {
+			'zh-Hans': {
+				title: {
+					links: '友情链接',
+				},
+			},
+		},
+		globalComponents: ['TrmDividerTitle'],
+		topBars: [
+			{ title: 'Home', url: '/' },
+			{
+				title: 'Demo',
+				children: [
+					{ title: '归档', url: '/archives' },
+					{ title: '分类', url: '/categories' },
+					{ title: '标签', url: '/tags' },
+				],
+			},
+			{ title: 'Links', url: '/links' },
+			{ title: 'About', url: '/about' },
 		],
 		sidebar: {
-			'/': [
+			typedText: ['Web Developer', 'UI Designer'],
+			social: [
 				{
-					text: '指南',
-					items: [
-						{
-							text: '使用指南',
-							link: '/guide/',
-						},
-						{
-							text: '主题配置',
-							link: '/guide/config',
-						},
-						{
-							text: '更新日志',
-							link: '/guide/change-log',
-						},
-						{
-							text: 'FAQ',
-							link: '/guide/faq',
-						},
-					],
-				},
-				{
-					text: '关于',
-					items: [
-						{
-							text: '关于主题',
-							link: '/guide/about',
-						},
-					],
+					name: 'vue',
+					url: 'https://cn.vuejs.org/',
+					icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M356.9 64.3H280l-56 88.6-48-88.6H0L224 448 448 64.3h-91.1zm-301.2 32h53.8L224 294.5 338.4 96.3h53.8L224 384.5 55.7 96.3z"/></svg>`,
 				},
 			],
 		},
+		user: {
+			email: 'admin@imalun.com',
+		},
 		footer: {
-			message: 'Released under the SATA | MIT License.',
-			copyright: 'Copyright © 2023-PRESENT MaLuns',
+			copyrightYear: '2023',
+			liveTime: {
+				enable: true,
+				startTime: '2023/10/01',
+			},
+		},
+		links: [
+			{
+				name: 'Vitepress',
+				url: 'https://vitepress.dev',
+				image: 'https://vitepress.dev/vitepress-logo-mini.svg',
+				desc: 'VitePress is a Static Site Generator (SSG) designed for building fast, content-centric websites',
+			},
+			{
+				name: '白云苍狗',
+				url: 'https://www.imalun.com/',
+				image: 'https://www.imalun.com/images/avatar.jpg',
+				desc: '醒，亦在人间；梦，亦在人间',
+			},
+		],
+		reward: {
+			enable: true,
+			methods: [
+				{
+					name: '微信',
+					path: 'https://www.imalun.com/images/reward/wx_pay.jpg',
+				},
+				{
+					name: '支付宝',
+					path: 'https://www.imalun.com/images/reward/alipay.jpg',
+				},
+				{
+					name: 'QQ',
+					path: 'https://www.imalun.com/images/reward/qq_pay.jpg',
+				},
+			],
+		},
+		rightside: {
+			readmode: true,
+			aside: true,
 		},
 		outline: {
-			level: [2, 4],
+			level: [2, 6],
+		},
+		favicon: {
+			logo: '/favicon.svg',
+			icon16: '/favicon.svg',
+			icon32: '/favicon.svg',
+			visibilitychange: true,
+		},
+		creativeCommons: {
+			post: true,
+			clipboard: true,
+		},
+		search: {
+			provider: 'local',
+		},
+		page: {
+			archives: '/archives',
+			tags: '/tags',
+			categorys: '/categories',
+		},
+		noticeOutdate: {
+			enable: true,
+			limitDay: 1,
+			style: 'flat',
+			position: 'bottom',
+		},
+		rss: {
+			enable: true,
+			baseUrl: 'https://vitepress-theme-async.imalun.com',
 		},
 	},
 });
--- docs/index.md
+++ docs/index.md
@@ -1,34 +1,6 @@
 ---
-title: 主页
-layout: home
-
-hero:
-  name: Blog-Theme-Async
-  image:
-    src: /logo.gif
-  tagline: 🎈 A simple & lightweight theme for Vitepress.
-  actions:
-    - text: 🚀 Get Started
-      link: /guide/
-    - text: 💻 Online demo
-      link: https://stackblitz.com/edit/vitejs-vite-fwgwrx?embed=1&theme=dark&view=editor
-      theme: alt
-
-features:
-  - title: 简洁
-    icon: 🌸
-    details: 简洁、优雅、轻量、美观的用户界面。
-  - title: 响应式
-    icon: ⚡️
-    details: 响应式界面，支持手机、平板、PC 多种平台。
-  - title: 极速
-    icon: 🚀
-    details: 基于 Vitepress 构建更快速。
-
-footer:
-  license: MIT Licensed
-  since: 2022
-  author:
-    name: MaLuns
-    url: https://www.imalun.com
+title: 首页
+index: true
+banner:
+	bannerTitle: 树深时见鹿，<br>溪午不闻钟。
 ---
