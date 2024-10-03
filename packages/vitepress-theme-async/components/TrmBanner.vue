49c8b19065271705a4fda103aae9558612092535
Shan <bliu699@outlook.com>
2024-10-03 10:16:48 +0800

    Refactor TrmBanner component for improved readability and maintainability

--- packages/vitepress-theme-async/components/TrmBanner.vue
+++ packages/vitepress-theme-async/components/TrmBanner.vue
@@ -8,46 +8,50 @@ const banner = useBanner();
 const singleColumn = useSingleColumn();
 
 const onScrollTo = () => {
-	const container = document.querySelector(".trm-banner");
-	container && window.scrollTo({ top: container.clientHeight - 20, behavior: "smooth" });
+  const container = document.querySelector(".trm-banner");
+  container && window.scrollTo({ top: container.clientHeight - 20, behavior: "smooth" });
 };
 </script>
 
 <template>
-	<div class="trm-banner">
-		<slot name="banner-top" />
-		<TrmBannerBg :banner="banner" />
-		<div class="trm-banner-content trm-overlay">
-			<div class="container">
-				<div class="row">
-					<div v-if="!singleColumn" class="col-lg-4"></div>
-					<div :class="!singleColumn ? 'col-lg-8' : 'col-lg-12'">
-						<!-- banner title -->
-						<div :class="!singleColumn ? 'trm-banner-text' : 'trm-banner-text trm-text-center'">
-							<div class="trm-label trm-mb-20">{{ banner?.bannerText }}</div>
-							<h1 class="trm-mb-30 trm-hsmb-font" v-html="banner?.bannerTitle"></h1>
-							<ul class="trm-breadcrumbs trm-label">
-								<li>
-									<a :href="withBase('/')" class="trm-anima-link">Home</a>
-								</li>
-								<li>
-									<span>
-										{{ page.relativePath.split("/")[0] }}
-									</span>
-								</li>
-							</ul>
-						</div>
-						<!-- banner title end -->
-						<!-- scroll hint -->
-						<span class="trm-scroll-hint-frame" @click="onScrollTo">
-							<div class="trm-scroll-hint"></div>
-							<span class="trm-label">Scroll down</span>
-						</span>
-						<!-- scroll hint end -->
-					</div>
-				</div>
-			</div>
-		</div>
-		<slot name="banner-bottom" />
-	</div>
+  <div class="trm-banner">
+    <slot name="banner-top" />
+    <TrmBannerBg :banner="banner" />
+    <div class="trm-banner-content trm-overlay">
+      <div class="container">
+        <div class="row">
+          <div v-if="!singleColumn"
+               class="col-lg-4"></div>
+          <div :class="!singleColumn ? 'col-lg-8' : 'col-lg-12'">
+            <!-- banner title -->
+            <div :class="!singleColumn ? 'trm-banner-text' : 'trm-banner-text trm-text-center'">
+              <div class="trm-label trm-mb-20">{{ banner?.bannerText }}</div>
+              <h1 class="trm-mb-30 trm-hsmb-font"
+                  v-html="banner?.bannerTitle"></h1>
+              <ul class="trm-breadcrumbs trm-label">
+                <li>
+                  <a :href="withBase('/')"
+                     class="trm-anima-link">Home</a>
+                </li>
+                <li>
+                  <span>
+                    {{ page.relativePath.split("/")[0] }}
+                  </span>
+                </li>
+              </ul>
+            </div>
+            <!-- banner title end -->
+            <!-- scroll hint -->
+            <span class="trm-scroll-hint-frame"
+                  @click="onScrollTo">
+              <div class="trm-scroll-hint"></div>
+              <span class="trm-label">Scroll down</span>
+            </span>
+            <!-- scroll hint end -->
+          </div>
+        </div>
+      </div>
+    </div>
+    <slot name="banner-bottom" />
+  </div>
 </template>
