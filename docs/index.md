5274e0b789b12f7ec72292fd37d6dab94da587fb
Shan <bliu699@outlook.com>
2024-10-03 09:54:44 +0800

    docs(index): add single_column layout option

--- docs/index.md
+++ docs/index.md
@@ -1,6 +1,7 @@
 ---
 title: 首页
 index: true
+single_column: true
 banner:
 	bannerTitle: 树深时见鹿，<br>溪午不闻钟。
 ---
--- packages/vitepress-theme-async/components/TrmPageIndex.vue
+++ packages/vitepress-theme-async/components/TrmPageIndex.vue
@@ -17,28 +17,40 @@ const allPosts = useAllPosts();
 const pageSize = theme.value.indexGenerator?.perPage || 10;
 
 const categories = useCategories()
-	.sort((a, b) => b[1] - a[1])
-	.slice(0, 2);
+  .sort((a, b) => b[1] - a[1])
+  .slice(0, 2);
 
 const pageList = computed(() => {
-	const startIdx = (currentPageIndex.value - 1) * pageSize;
-	const endIdx = startIdx + pageSize;
-	return allPosts.slice(startIdx, endIdx);
+  const startIdx = (currentPageIndex.value - 1) * pageSize;
+  const endIdx = startIdx + pageSize;
+  return allPosts.slice(startIdx, endIdx);
 });
 </script>
 <template>
-	<div v-if="categories.length > 0" class="row hidden-sm">
-		<div v-for="(item, index) in categories" class="col-lg-6" :key="index">
-			<TrmCardCategorie :name="item[0]" :length="item[1]" :category-url="pageUrl.categorys + '?q=' + item[0]" />
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-lg-12" v-if="categories.length > 0">
-			<TrmDividerTitle :title="$t('title.newPublish')" index="01" />
-		</div>
-		<div class="col-lg-6" v-for="item in pageList" :key="item.url">
-			<TrmCardPost :post="item" :category-url="pageUrl.categorys" :sticky="page.frontmatter.index && item.sticky && item.sticky > 0" />
-		</div>
-	</div>
-	<TrmPagination :total="allPosts.length" :size="pageSize" />
+  <div v-if="categories.length > 0"
+       class="row hidden-sm">
+    <div v-for="(item, index) in categories"
+         class="col-lg-6"
+         :key="index">
+      <TrmCardCategorie :name="item[0]"
+                        :length="item[1]"
+                        :category-url="pageUrl.categorys + '?q=' + item[0]" />
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-lg-12"
+         v-if="categories.length > 0">
+      <TrmDividerTitle :title="$t('title.newPublish')"
+                       index="01" />
+    </div>
+    <div class="col-lg-4"
+         v-for="item in pageList.slice(0, 3)"
+         :key="item.url">
+      <TrmCardPost :post="item"
+                   :category-url="pageUrl.categorys"
+                   :sticky="page.frontmatter.index && item.sticky && item.sticky > 0" />
+    </div>
+  </div>
+  <TrmPagination :total="allPosts.length"
+                 :size="pageSize" />
 </template>
